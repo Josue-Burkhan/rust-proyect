c5e6e9a8c0f0ee6a37182f8e5274b1b74b5eba1f
joshua.burkhan <bai21030@byui.edu>
2025-07-12 13:17:28 -0500

    Rust Proyect V1.0.0

---
+++ new_world/target/debug/deps/new_world-aabf24c355e818f1.d
@@ -0,0 +1,5 @@
+E:\BYUI\CSE310\Rust\rust-proyect\new_world\target\debug\deps\new_world-aabf24c355e818f1.d: src\main.rs
+
+E:\BYUI\CSE310\Rust\rust-proyect\new_world\target\debug\deps\libnew_world-aabf24c355e818f1.rmeta: src\main.rs
+
+src\main.rs:
---
+++ new_world/target/debug/deps/new_world-db8cb87d497f5e30.d
@@ -0,0 +1,5 @@
+E:\BYUI\CSE310\Rust\rust-proyect\new_world\target\debug\deps\new_world-db8cb87d497f5e30.d: src\main.rs
+
+E:\BYUI\CSE310\Rust\rust-proyect\new_world\target\debug\deps\libnew_world-db8cb87d497f5e30.rmeta: src\main.rs
+
+src\main.rs:
